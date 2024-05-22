b852d279a20ec22e719dd0ae5b7a57c62ac56412
aisw-arya <aiswaryas0701@gmail.com>
2024-05-22 15:23:24 +0530

    Created print statement and arithmetic operations

---
+++ ex3.py
@@ -0,0 +1,24 @@
+print("I will now count my chickens:") # Print statement to indicate counting chickens. 
+
+print("Hens", 25 + 30 / 6)             # print "Hens" and calculate the number of hens using arithmetic operators.
+print("Roosters", 100 - 25 * 3 % 4)    # print "Roosters" and calculate the number of roosters using arithmetic operators.
+
+print("Now I will count the eggs: ")    #Print statement to indicate counting of eggs.
+
+print(3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6)
+
+print("Is it true that 3 + 2 < 5 - 7?") # Print the question.  
+
+print(3 + 2 < 5 - 7)   # Print the result of arithmetic comparison. 
+
+print("What is 3 +2 ?",3 + 2)  # Print the question and it's result. 
+print("What is 5 -7?", 5 -7)  # Print the question and it's result.
+
+print("Oh, that's why it's False.")   #Print statement explaining the previous comparison result. 
+
+print("How about some more.") #Print statement to indicate continuation.
+
+print("Is it greater?", 5 > -2)       # Perform and print additional comparison operations.
+print("Is it greater or equal ?", 5 >= -2)   # Perform and print additional comparison operations.
+print("Is it less or equal ?", 5 <= -2)     # Perform and print additional comparison operations.
+
