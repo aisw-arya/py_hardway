020606b81f8a1f256220abc5ba243306ad002e6f
aisw-arya <aiswaryas0701@gmail.com>
2024-05-23 12:04:11 +0530

    Created code for variable initializing

---
+++ ex9.py
@@ -0,0 +1,15 @@
+#Here's some new strange stuff, remember type it exactly
+
+days = "Mon Tue Wed Thu Fri Sat Sun"  #initializing variable days with values 
+months = "Jan\nFeb\nMar\nApril\nMay\nJUn\Jul\nAug"  #initializing the variable months with values
+
+print("Here are the days: ",days)  #print values in the varibale days
+print("Here are the months: ",months) #print values in the variable months
+
+print("""
+There's something going on there.
+with the three double-quotes.
+we'll be able tp type as much as we like.
+Even 4 lines if we want, or 5, or 6.
+"""
+)
