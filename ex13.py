2b1578f3159aff523a2d9ac0e5203d6e9f7d0160
aisw-arya <aiswaryas0701@gmail.com>
2024-05-24 10:39:48 +0530

    Added code to from unpack command-line arguments and print them out using argv.

---
+++ ex13.py
@@ -0,0 +1,6 @@
+from sys import argv     #import argv.
+script, first, second, third = argv #Unpack the command-line arguments passed to the script into variables: script, first, second, and third.
+print("The script is called:", script) #Print out the name of your python file.
+print("your first variable is:", first)  #Print out the value of first argument.
+print("your second variable is:", second)
+print("your third variable is:", third)
