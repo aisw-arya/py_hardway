32b617b739e648e4a0776925e10696a7fc492558
aisw-arya <aiswaryas0701@gmail.com>
2024-05-30 23:04:54 +0530

    Create code for understanding the logic operators.

---
+++ ex29.py
@@ -0,0 +1,29 @@
+people = 20
+cats = 30
+dogs = 15
+
+
+if people < cats:       #to check whether no. of people  less than no.of cats
+    print("Too many cats! The world is doomed!")
+
+if people > cats:          #to check whether no. of people  greater than no.of cats
+    print("Not many cats! The world is saved!")
+
+if people < dogs:           #to check whether no. of people  less than no.of dogs
+    print("The world is drooled on!")
+
+if people > dogs:           #to check whether no. of people  greater than no.of dogs
+    print("The world is dry!")
+
+
+dogs += 5      #incrementing number of dog
+
+if people >= dogs:              #to check whether no. of people  greater than or equal to no.of dogs
+    print("People are greater than or equal to dogs.")    
+
+if people <= dogs:          #to check whether no. of people  less than or equal to no.of dogs
+    print("People are less than or equal to dogs.")
+
+
+if people == dogs:     #to check whether no. of people   equal to no.of dogs
+    print("People are dogs.")
